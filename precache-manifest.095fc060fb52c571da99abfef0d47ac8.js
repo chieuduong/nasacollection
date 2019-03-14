96f1fd55f715d8b41ec90f5922366a6df5188835
Chieu Duong <chieu.ens@gmail.com>
2019-03-14 20:10:38 +0700

    Updates

---
+++ precache-manifest.095fc060fb52c571da99abfef0d47ac8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "51fa87b4e8853943d0942922442c7af7",
+    "url": "/nasacollection/static/media/add.51fa87b4.svg"
+  },
+  {
+    "revision": "1a275508a5ea543d6666",
+    "url": "/nasacollection/static/css/main.a9575335.chunk.css"
+  },
+  {
+    "revision": "b66acab10fd81d056bda",
+    "url": "/nasacollection/static/js/runtime~main.13852f2d.js"
+  },
+  {
+    "revision": "8b6e533ac95ebeb475a2",
+    "url": "/nasacollection/static/js/2.bfa8526d.chunk.js"
+  },
+  {
+    "revision": "556da207fbd0bd5459e851ee4a4778f1",
+    "url": "/nasacollection/static/media/heart.556da207.svg"
+  },
+  {
+    "revision": "2aab9269f85782debc2ef5be4daa3a27",
+    "url": "/nasacollection/static/media/favorite-heart-button.2aab9269.svg"
+  },
+  {
+    "revision": "1a275508a5ea543d6666",
+    "url": "/nasacollection/static/js/main.02a7af43.chunk.js"
+  },
+  {
+    "revision": "40a6d365dd3b8fa31fc9e90499631298",
+    "url": "/nasacollection/static/media/tick.40a6d365.svg"
+  },
+  {
+    "revision": "e7968bc7855f2380ff042c5b5d24b341",
+    "url": "/nasacollection/static/media/close.e7968bc7.svg"
+  },
+  {
+    "revision": "ffee154c12840eb6ca93b91eace1d339",
+    "url": "/nasacollection/static/media/edit.ffee154c.svg"
+  },
+  {
+    "revision": "99e3c7ec332864aa3e5701eaf756506b",
+    "url": "/nasacollection/static/media/delete.99e3c7ec.svg"
+  },
+  {
+    "revision": "31b44a6764213fbdeb62bb4024766deb",
+    "url": "/nasacollection/static/media/back.31b44a67.svg"
+  },
+  {
+    "revision": "7eaace3bd7ed4a0ebbf6081ad268d7f5",
+    "url": "/nasacollection/index.html"
+  }
+];
