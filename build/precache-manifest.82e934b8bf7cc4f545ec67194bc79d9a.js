36fd9ca4f1cbcb7ef60c52daba7af19eebd0762d
Chieu Duong <chieu.ens@gmail.com>
2019-03-14 19:36:30 +0700

    deploy build

---
+++ build/precache-manifest.82e934b8bf7cc4f545ec67194bc79d9a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "2aab9269f85782debc2ef5be4daa3a27",
+    "url": "/static/media/favorite-heart-button.2aab9269.svg"
+  },
+  {
+    "revision": "79025bac3eceff012c71",
+    "url": "/static/css/main.6c70da01.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "8b6e533ac95ebeb475a2",
+    "url": "/static/js/2.bfa8526d.chunk.js"
+  },
+  {
+    "revision": "556da207fbd0bd5459e851ee4a4778f1",
+    "url": "/static/media/heart.556da207.svg"
+  },
+  {
+    "revision": "51fa87b4e8853943d0942922442c7af7",
+    "url": "/static/media/add.51fa87b4.svg"
+  },
+  {
+    "revision": "79025bac3eceff012c71",
+    "url": "/static/js/main.02a7af43.chunk.js"
+  },
+  {
+    "revision": "e7968bc7855f2380ff042c5b5d24b341",
+    "url": "/static/media/close.e7968bc7.svg"
+  },
+  {
+    "revision": "40a6d365dd3b8fa31fc9e90499631298",
+    "url": "/static/media/tick.40a6d365.svg"
+  },
+  {
+    "revision": "ffee154c12840eb6ca93b91eace1d339",
+    "url": "/static/media/edit.ffee154c.svg"
+  },
+  {
+    "revision": "31b44a6764213fbdeb62bb4024766deb",
+    "url": "/static/media/back.31b44a67.svg"
+  },
+  {
+    "revision": "99e3c7ec332864aa3e5701eaf756506b",
+    "url": "/static/media/delete.99e3c7ec.svg"
+  },
+  {
+    "revision": "a6227e986f0335d02c6c214089456546",
+    "url": "/index.html"
+  }
+];
